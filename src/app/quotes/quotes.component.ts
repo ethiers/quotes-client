1b98478c26a0ef757897f043dba99f920aab0766
Sebastien Ethier <sebastien.ethier@seconceptweb.ca>
2019-11-30 22:41:10 -0500

    Section 6: Working on the UI

---
+++ src/app/quotes/quotes.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-quotes',
+  templateUrl: './quotes.component.html',
+  styleUrls: ['./quotes.component.scss']
+})
+export class QuotesComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+
+    return {background: color};
+  }
+
+}
